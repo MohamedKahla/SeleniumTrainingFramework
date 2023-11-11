9d0fae125ed180d1d0c9bc35652b51f4f1261376
MohamedKahla <kahla.med90@gmail.com>
2023-11-11 16:53:31 +0100

    add user

---
+++ src/main/java/intrTrainingSeleniumWithRahul/pageObject/UserVerify.java
@@ -0,0 +1,26 @@
+package intrTrainingSeleniumWithRahul.pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+import intrTrainingSeleniumWithRahul.AbstractComponent.AbstractComponent;
+
+public class UserVerify extends AbstractComponent {
+
+	public UserVerify(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
